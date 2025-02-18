19eed34f18f4e2e0c85d8e627f595aff3485de41
ChEricPaquet <2437295@cegepba.qc.ca>
2025-02-18 10:56:15 -0500

    ;io;i;

--- PO1_Encapsulation/Class1.cs
+++ PO1_Encapsulation/Class1.cs
@@ -10,8 +10,60 @@ namespace PO1_Encapsulation
     {
         int pointDeVie;
         int puissanceAttaque;
+        int pointDeVieMaximal;
     
-        
+        public int PointDeVie
+        {
+            get => PointDeVie1; set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                else if (value > PointDeVieMaximal)
+                {
+                    PointDeVie1 = PointDeVieMaximal;
+                }
+                else
+                {
+                    PointDeVie1 = value;
+                }
+
+            }
+        }
+                
+        public int PuissanceAttaque { get => PuissanceAttaque; set { PuissanceAttaque = value; } }
+
+        public bool EstMort
+        {
+            get
+            {
+                if (PointDeVie1 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+        }
+
+        public int PointDeVie1 { get => PointDeVie2; set => PointDeVie2 = value; }
+        public int PuissanceAttaque1 { get => PuissanceAttaque2; set => PuissanceAttaque2 = value; }
+        public int PointDeVieMaximal { get => PointDeVieMaximal1; set => PointDeVieMaximal1 = value; }
+        public int PointDeVie2 { get => pointDeVie; set => pointDeVie = value; }
+        public int PuissanceAttaque2 { get => puissanceAttaque; set => puissanceAttaque = value; }
+        public int PointDeVieMaximal1 { get => pointDeVieMaximal; set => pointDeVieMaximal = value; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Personnage personnage &&
+                   PointDeVie2 == personnage.PointDeVie2 &&
+                   PuissanceAttaque2 == personnage.PuissanceAttaque2 &&
+                   PointDeVieMaximal1 == personnage.PointDeVieMaximal1;
+        }
     }
 
 }
--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,14 +2,44 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur;
+        Personnage monstre;
         public Form1()
         {
             InitializeComponent();
+            joueur = new Personnage();
+            joueur.PointDeVieMaximal = 10;
+            joueur.PointDeVie = 10;
+            joueur.PuissanceAttaque = 1;
+            
+            
+            monstre = new Personnage();
+            monstre.PointDeVieMaximal = 30;
+            monstre.PointDeVie = 30;
+            monstre.PuissanceAttaque = 3;
+           
+
+            lbl_pointsVieJoueur.Text = "Point de vie " + joueur.PointDeVie;
+            lbl_puissanceAttaqueJoueur.Text = "Puissance d'attaque " + joueur.PuissanceAttaque;
+            lbl_pointsVieDragon.Text = "Point de vie " + monstre.PointDeVie;
+            lbl_puissanceAttaqueDragon.Text = "Puissance d'attaque " + monstre.PuissanceAttaque;
+            actualiserAffichage();
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
+            monstre.PointDeVie = monstre.PointDeVie - joueur.PuissanceAttaque;
+            joueur.PointDeVie = joueur.PointDeVie - monstre.PuissanceAttaque;
 
+            actualiserAffichage();
+            if (joueur.EstMort)
+            {
+                MessageBox.Show("Vous êtes mort");
+            }
+            if (monstre.EstMort)
+            {
+                MessageBox.Show("Vous avez gagner");
+            }
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
@@ -19,6 +49,13 @@ namespace PO1_Encapsulation
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+
+        }
+        private void actualiserAffichage()
+        {
+            lbl_pointsVieJoueur.Text = "Point de vie " + joueur.PointDeVie;
+            lbl_puissanceAttaqueJoueur.Text = "Puissance d'attaque " + joueur.PuissanceAttaque;
+            lbl_pointsVieDragon.Text = "Point de vie " + monstre.PointDeVie;
 
         }
     }
